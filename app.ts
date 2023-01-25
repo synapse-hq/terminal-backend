0de74efad0cf17025f6a3cdd910f154666ee5af4
Diego HR <dhernandez2448@gmail.com>
2023-01-25 16:43:07 -0500

    login api

--- app.ts
+++ app.ts
@@ -4,9 +4,13 @@ import { pg } from './src/db'
 import { mongo } from './src/db'
 // import { prisma } from "./src/generated/pg";
 import * as dotenv from "dotenv"
+
+
 dotenv.config();
 
 const port = process.env.PORT;
+
+
 
 // using the non-null assertion operator !
 const domain:string = process.env.DOMAIN!;
@@ -14,6 +18,11 @@ import rootDomainRoutes from './routes/rootdomain_route';
 import subDomainRoutes from './routes/subdomain_route';
 
 const app: Application = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static('build'));
 
 
 app.use(vhost(domain, rootDomainRoutes))
@@ -22,35 +31,35 @@ app.use(vhost(domain, rootDomainRoutes))
     .use(vhost('*.' + domain, subDomainRoutes));
 
 async function main() {
-  // const user = await pg.user.create({
-  //   data: {
-  //     username: 'synapse',
-  //     passwordHash: 'asjldfnsdafde',
-  //     createdAt: new Date(),
-  //   },
-  // });
+  const user = await pg.user.create({
+    data: {
+      username: 'synapse',
+      passwordHash: 'asjldfnsdafde',
+      createdAt: new Date(),
+    },
+  });
 
   // console.log(user);
 
   // const users = await pg.user.findMany();
   // console.log(users);
 
-  const request = {
-    body: 'here is the body',
-    headers: {
-      first: 'firstheader',
-      second: 'secondheader',
-    },
-  };
+  // const request = {
+  //   body: 'dego test',
+  //   headers: {
+  //     first: 'firstheader',
+  //     second: 'secondheader',
+  //   },
+  // };
 
-  await mongo.payload.create({ data: { request } });
+  // await mongo.payload.create({ data: { request } });
 
 }
 
-main().catch(e => console.error(e.message))
-      .finally(async () => {
-        await pg.$disconnect();
-        await mongo.$disconnect();
-      })
+// main().catch(e => console.error(e.message))
+//       .finally(async () => {
+//         await pg.$disconnect();
+//         await mongo.$disconnect();
+//       })
 
 app.listen(port, () => console.log('Server listening on port '+ port));
--- routes/rootdomain_route.ts
+++ routes/rootdomain_route.ts
@@ -2,11 +2,100 @@ import express, { Request, Response, Application } from "express";
 const router = express.Router();
 import { pg } from '../src/db'
 import { mongo } from '../src/db'
+const bcrypt = require("bcrypt")
 
 
 // user attempts to log in
+type userReqBody = {
+  username: string,
+  passwordHash: string
+}
 
-router.post('/login')
+router.post('/new_user', async (req : Request, res: Response) => {
+  console.log(req)
+  const body : userReqBody = req.body
+  const {username, passwordHash} = body;
+
+  console.log(body, username)
+  let user = await pg.user.findFirst({
+    where: {
+      username: username,
+    },
+  })
+
+  console.log(user)
+  if (user) {
+    res.status(404).json({error: "Username already exists"})
+    return
+  }
+
+  if (passwordHash.length < 5) {
+    res.status(404).json({error: "Invalid Password"})
+    return
+  }
+
+  const saltRounds = 10;
+  // console.log(password, saltRounds)
+  const hashed: string = await bcrypt.hash(passwordHash, saltRounds);
+
+  try {
+    const user = await pg.user.create({ 
+      data: {
+        username: username, 
+        passwordHash: hashed,
+        createdAt:  new Date()
+      },
+    });
+    // const savedUser = await user.save();
+    res.status(201).json(user);
+  } catch(err) {
+    res.status(404).json({error: 'username already in use'})
+
+  }
+});
+
+
+// login
+router.post('/login', async (req : Request, res : Response) => {
+  const body : userReqBody = req.body
+  const {username, passwordHash} = {...body};
+
+  // let data = await User.find({});
+  let user = await pg.user.findFirst({
+    where: {
+      username: username,
+    },
+  })
+
+  if (user) {
+    let validCredentials = await bcrypt.compare(passwordHash, user.passwordHash);
+    console.log("passwords", user.passwordHash, passwordHash)
+    
+    if (validCredentials) {
+      let buckets = await pg.bucket.findMany({
+        where: {
+          userId: user.id
+        }
+      })
+      res.send({username, buckets,})
+    } else {
+      res.status(404).json({error: "invalid password"});
+    }
+  } else {
+    res.status(404).json({error: 'user does not exist'});
+  }
+});
+
+router.delete('/:username', async (req: Request, res: Response) => {
+  const deleteUsers = await pg.user.delete({
+    where: {
+      username: req.params.username
+    }
+  })
+
+  console.log(deleteUsers)
+  res.json(deleteUsers)
+});
 
 // do we need this route?
 router.get('/', async (req, res, next) => {
