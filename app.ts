93cccd987e0d67bbfc53ff8248b4dbbd3769540b
Diego HR <dhernandez2448@gmail.com>
2023-01-28 15:09:16 -0500

    creating backend sockets connection
    Co-authored-by: Mykolas Viningas <m.viningas@gmail.com>
    Co-authored-by: Yusuf Birader <yusuf.birader@hotmail.com>
    Co-authored-by: Derek Bruinooge <derek.bruinooge@gmail.com>

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
 import express, { Request, Response, Application, NextFunction } from "express";
+import { pg } from "./src/db"
+import { mongo } from "./src/db"
+
 import vhost = require('vhost')
-import { pg } from './src/db'
-import { mongo } from './src/db'
 import * as dotenv from "dotenv"
+
 dotenv.config();
+
+const port = process.env.PORT;
+const domain:string = process.env.DOMAIN!;
+
+import subDomainRoutes from './routes/subdomain_route';
+import rootDomainRoutes from "./routes/rootDomainRoutes"
 
 //sessions
 const session = require("express-session");
@@ -12,13 +20,83 @@ const { createClient } = require('redis')
 let redisClient = createClient({ legacyMode: true })
 redisClient.connect().catch(console.error);
 
-const port = process.env.PORT;
-const domain:string = process.env.DOMAIN!;
 
-import subDomainRoutes from './routes/subdomain_route';
-import rootDomainRoutes from "./routes/rootDomainRoutes"
+// sockets
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
 const app: Application = express();
+
+// sockets
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server, path: "/buckets"})
+
+// when connection established
+wss.on('connection', (ws: WebSocket) => {
+
+  //// logic for retrieving request for a given for subdomain
+    const getReqs = async(subdomain: string) => {
+      subdomain = subdomain.toString()
+      console.log("SUBDOMAIN VAL IN ASYNC FUNC", subdomain)
+      
+      const bucket = await pg.bucket.findFirst({
+        where: {
+          subdomain,
+        }
+      })
+    
+      if (!bucket) {
+        console.log("NO BUCKET FOUND")
+        ws.send("NO BUCKET FOUND")
+        return
+      }
+    
+      let requests = await pg.request.findMany({
+        where: {
+          bucketId: bucket.id,
+        }
+      })
+      console.log(requests, "BEFORE getting raw request")
+      try {
+        let promises = requests.map(async(request) => {
+          const payload = await mongo.payload.findUnique({
+            where: {
+              id: request.payload
+            }
+          })
+    
+          return {
+            ...request,
+            rawRequest: payload?.rawRequest
+          }
+        })
+    
+        requests = await Promise.all(promises)
+      } catch {
+        ws.send("request retrieval failed")
+        console.log("reqs retrieval failed")
+      }
+    
+      ws.send(JSON.stringify(requests));
+    }
+  ////
+
+    //function executed when websocket on server receives a message
+    ws.on('message', (subdomain: string) => {
+
+        //log the received message
+        console.log('bucket subdomain: %s', subdomain);
+        console.log("bucket requests: \n\n")
+
+        // execute reqs retrieval function
+        getReqs(subdomain)
+    });
+
+    // initial connection acknowledgement on client and server side
+    console.log("websocket connection established")
+    ws.send("websocket connection established")
+});
+//
 const cors = require('cors');
 
 app.use(express.json());
@@ -40,6 +118,7 @@ app.use(session({
 
 console.log(domain)
 
+
 app.use(vhost(domain, rootDomainRoutes));
 app.use(vhost("www." + domain, rootDomainRoutes));
 app.use(vhost("*." + domain, subDomainRoutes));
@@ -48,4 +127,12 @@ app.use((req: Request, res: Response) => {
   res.send("END OF ROUTES")
 });
 
-app.listen(port, () => console.log('Server listening on port '+ port));
+server.listen(port, () => console.log('Server listening on port '+ port));
+
+
+
+
+
+
+
+
